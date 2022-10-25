5acd2ebaa896fd3277d2b83fd22d6ca1a8d5e2fc
Liv Nelson <livnelson@me.com>
2022-10-25 14:53:52 -0400

    joke generator, diplay joke, cleaned up functions

--- index.js
+++ index.js
@@ -3,28 +3,50 @@ const jokeCategory = "christmas"
 fetch(`https://v2.jokeapi.dev/joke/${jokeCategory}?amount=10?format=json`)
 .then(resp => resp.json())
 .then(jokeData => {
-  //console.log(jokeData);
+  // console.log(jokeData);
   console.log(jokeData.jokes)
-  renderJokeButtons(jokeData)
-//getJokes(jokeData.jokes)
+  // renderJokeButtons(jokeData)
+  // renderJod(jokeData.jokes)
+  determineJokeType(jokeData.jokes[0])
+})
 }
-  )
-}
-  //If joke is two part return set/delivery. if joke is onepart return joke body
-  function getJokes(jokeArray) {
-    for(joke of jokeArray) {
-      //console.log(joke);
-      if (joke.type === "twopart") {
-        console.log(`SETUP ${joke.setup} DELIVERY ${joke.delivery}`)
-      }
-      else {console.log(`JOKE ${joke.joke}`)}
-  }}
-  randomJokeFetch()
 
+//If joke is two part return set/delivery. if joke is onepart return joke body
+
+/* <h2 id="setup"></h2>
+<p id="delivery"></p>
+<h2 id="jod"></h2>
+<p id="joke-category"></p> */
+
+function jokesLoop(jokeArray) {
+    for(joke of jokeArray) {
+      determineJokeType(joke)}}
+      //console.log(joke);
+
+function renderTwoPart(joke) {
+    const setupJod = document.getElementById('setup')
+    const deliveryJod = document.getElementById('delivery')
+    setupJod.textContent = `${joke.setup}`
+    deliveryJod.textContent = `${joke.delivery}`
+}
+
+function renderOnePart(joke){
+  const jokeJod = document.getElementById('jod')
+  jokeJod.textContent = `${joke.joke}`
+}
+
+function determineJokeType(joke) {
+  if (joke.type === "twopart") {
+    renderTwoPart(joke)
+    // console.log(`SETUP ${joke.setup}, DELIVERY ${joke.delivery}`)
+  } else {
+    renderOnePart(joke)
+    // console.log(`JOKE ${joke.joke}`)
+}}
 
   //render joke buttons.
 
-  function renderJokeButtons(jokeData) {
+function renderJokeButtons(jokeData) {
     const catButtonList = document.createElement("ul")  //creating html list
     const buttonDiv = document.getElementById("joke-categories-menu")
     buttonDiv.append(catButtonList)
@@ -33,4 +55,19 @@ fetch(`https://v2.jokeapi.dev/joke/${jokeCategory}?amount=10?format=json`)
       jokeButton.textContent = joke.category
       catButtonList.append(jokeButton)
     })
-  }
+}
+
+randomJokeFetch()
+
+// random joke button 
+
+// const randomJoke = () => {
+//     fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+//     .then(resp => resp.json())
+//     .then(joke =>
+//       displayJoke())
+// }
+
+// display Joke
+
+
